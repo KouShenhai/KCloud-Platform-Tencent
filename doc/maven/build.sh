9d414645405be7be3c3c30a5c5131db7db40911c
koushenhai <2413176044@qq.com>
2022-10-11 11:10:07 +0800

    jdk1.8 替换成jdk17 问题修复（下个版本学习hmily，并自己写高性能的放在项目中）

--- doc/maven/build.sh
+++ doc/maven/build.sh
@@ -1,3 +1,5 @@
 clean package -Dmaven.test.skip=true -Pprod
 clean package -Dmaven.test.skip=true -Ptest
 clean package -Dmaven.test.skip=true -Pdev
+
+mvn -f pom.xml clean package -DskipTests docker:build
