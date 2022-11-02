7da492a7a833ae258dbe55c4fc7083e43257b969
koushenhai <2413176044@qq.com>
2022-11-02 13:43:36 +0800

    优化 移除微信公众号依赖

--- laokou-service/laokou-module/laokou-oss/laokou-oss-server/src/main/java/org/laokou/oss/server/OssApplication.java
+++ laokou-service/laokou-module/laokou-oss/laokou-oss-server/src/main/java/org/laokou/oss/server/OssApplication.java
@@ -3,6 +3,9 @@ package org.laokou.oss.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Kou Shenhai
+ */
 @SpringBootApplication
 public class OssApplication {
 
