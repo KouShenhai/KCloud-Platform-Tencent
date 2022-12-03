3a22248178e636ecbeae8aed4c0d490c1e5ffdfe
koushenhai <2413176044@qq.com>
2022-12-03 15:03:55 +0800

    add：gateway常量增加注释，增加kafka相关topic（kafka已开启新建topic）

--- laokou-cloud/laokou-gateway/src/main/java/org/laokou/gateway/constant/GatewayConstant.java
+++ laokou-cloud/laokou-gateway/src/main/java/org/laokou/gateway/constant/GatewayConstant.java
@@ -5,8 +5,45 @@ package org.laokou.gateway.constant;
  */
 public interface GatewayConstant {
 
+    /**
+     * 异常处理
+     */
     String SERVICE_MAINTENANCE_MSG = "服务正在维护，请联系管理员";
+    /**
+     * token鉴定
+     */
     String UNAUTHORIZED_MSG = "未授权";
+    /**
+     * 未知异常
+     */
     String OTHER_MSG = "其他的异常";
+    /**
+     * 服务熔断
+     */
     String FALLBACK_MSG = "服务已被降级熔断";
+
+    /**
+     * 请求链-用户id
+     */
+    String USER_ID = "userId";
+
+    /**
+     * 密码模式-请求地址
+     */
+    String OAUTH_URI = "/oauth/token";
+
+    /**
+     * 密码模式-用户名
+     */
+    String USERNAME = "username";
+
+    /**
+     * 密码模式-密码
+     */
+    String PASSWORD = "password";
+
+    /**
+     * 请求链topic
+     */
+    String LAOKOU_REQUEST_TRACE_TOPIC = "laokou_request_trace_topic";
 }
