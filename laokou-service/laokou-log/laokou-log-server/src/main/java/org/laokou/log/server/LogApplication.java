72b6f32178b0405160570dc687c06ad82e936697
koushenhai <2413176044@qq.com>
2022-12-09 13:01:34 +0800

    update：去掉重复代码

---
+++ laokou-service/laokou-log/laokou-log-server/src/main/java/org/laokou/log/server/LogApplication.java
@@ -0,0 +1,13 @@
+package org.laokou.log.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class, args);
+    }
+
+}
