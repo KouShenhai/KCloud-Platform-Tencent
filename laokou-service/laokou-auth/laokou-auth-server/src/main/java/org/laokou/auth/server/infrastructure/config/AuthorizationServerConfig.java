af812f544c2d5f713873192fc314f71c7e48723b
koushenhai <2413176044@qq.com>
2022-12-27 23:45:24 +0800

    style: auth增加证书

--- laokou-service/laokou-auth/laokou-auth-server/src/main/java/org/laokou/auth/server/infrastructure/config/AuthorizationServerConfig.java
+++ laokou-service/laokou-auth/laokou-auth-server/src/main/java/org/laokou/auth/server/infrastructure/config/AuthorizationServerConfig.java
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 package org.laokou.auth.server.infrastructure.config;
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
@@ -24,7 +25,6 @@ import org.laokou.auth.server.domain.sys.repository.service.SysMenuService;
 import org.laokou.auth.server.domain.sys.repository.service.impl.SysUserServiceImpl;
 import org.laokou.auth.server.infrastructure.customizer.CustomTokenCustomizer;
 import org.laokou.auth.server.infrastructure.handler.CustomAuthenticationFailureHandler;
-import org.laokou.auth.server.infrastructure.jwk.Jwk;
 import org.laokou.auth.server.infrastructure.log.LoginLogUtil;
 import org.laokou.auth.server.infrastructure.token.EmailAuthenticationToken;
 import org.laokou.auth.server.infrastructure.token.PasswordAuthenticationToken;
@@ -34,6 +34,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -48,12 +49,12 @@ import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.authorization.*;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
@@ -65,6 +66,15 @@ import org.springframework.security.oauth2.server.authorization.token.OAuth2Toke
 import org.springframework.security.oauth2.server.authorization.web.authentication.*;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
 import java.util.List;
 /**
@@ -122,7 +132,7 @@ public class AuthorizationServerConfig {
                 .clientSecret(passwordEncoder.encode("secret"))
                 // ClientAuthenticationMethod.CLIENT_SECRET_BASIC => client_id:client_secret 进行Base64编码后的值
                 // Headers Authorization Basic YXV0aC1jbGllbnQ6c2VjcmV0
-                // http://localhost:11111/oauth2/authorize?client_id=auth-client&client_secret=secret&response_type=code&scope=auth email phone&redirect_uri=https://www.baidu.com
+                // http://localhost:1111/oauth2/authorize?client_id=auth-client&client_secret=secret&response_type=code&scope=auth email phone&redirect_uri=https://www.baidu.com
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantTypes(authorizationGrantTypes -> authorizationGrantTypes.addAll(
                         List.of(AuthorizationGrantType.AUTHORIZATION_CODE
@@ -208,16 +218,39 @@ public class AuthorizationServerConfig {
         return daoAuthenticationProvider;
     }
 
+    /**
+     * 加载jwk资源
+     * 生成令牌
+     * @return
+     */
     @Bean
-    public JWKSource<SecurityContext> jwkSource() {
-        RSAKey rsaKey = Jwk.generateRsa();
+    public JWKSource<SecurityContext> jwkSource() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, JOSEException {
+        String alias = "auth";
+        String password = "koushenhai";
+        String path = "auth.jks";
+        ClassPathResource resource = new ClassPathResource(path);
+        KeyStore jks = KeyStore.getInstance("jks");
+        char[] pwd = password.toCharArray();
+        jks.load(resource.getInputStream(),pwd);
+        RSAKey rsaKey = RSAKey.load(jks, alias, pwd);
         JWKSet jwkSet = new JWKSet(rsaKey);
         return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
     }
 
+    /**
+     * JWT解码器
+     * 客户端认证授权后，需要访问用户信息，解码器可以从令牌中解析用户信息
+     * @return
+     * @throws CertificateException
+     */
     @Bean
-    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
-        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    JwtDecoder jwtDecoder() throws CertificateException, IOException {
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("x.509");
+        // 读取cer公钥证书来配置解码器
+        ClassPathResource resource = new ClassPathResource("auth.cer");
+        Certificate certificate = certificateFactory.generateCertificate(resource.getInputStream());
+        RSAPublicKey publicKey = (RSAPublicKey) certificate.getPublicKey();
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
     }
 
     @Bean
